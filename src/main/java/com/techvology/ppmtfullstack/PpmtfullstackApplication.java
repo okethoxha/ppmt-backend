d9fae080b3656b839c8417e15fadfe0b3d37c562
Oket <oket.hoxha@gmail.com>
2021-05-11 10:59:41 +0200

    User Registration pt.1

--- src/main/java/com/techvology/ppmtfullstack/PpmtfullstackApplication.java
+++ src/main/java/com/techvology/ppmtfullstack/PpmtfullstackApplication.java
@@ -2,9 +2,16 @@ package com.techvology.ppmtfullstack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class PpmtfullstackApplication {
+
+    @Bean
+    BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(PpmtfullstackApplication.class, args);
--- src/main/java/com/techvology/ppmtfullstack/services/UserService.java
+++ src/main/java/com/techvology/ppmtfullstack/services/UserService.java
@@ -1,7 +1,9 @@
 package com.techvology.ppmtfullstack.services;
 
+import com.techvology.ppmtfullstack.domain.User;
 import com.techvology.ppmtfullstack.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,4 +11,15 @@ public class UserService {
 
     @Autowired
     private UserRepository userRepository;  //TODO: Constructor injection to be done
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public User saveUser(User newUser){
+        newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
+
+
+
+        return userRepository.save(newUser);
+    }
 }
