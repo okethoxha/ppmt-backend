948acb499bfb9eb246a18712dbe3c0d902c58ee5
Oket <oket.hoxha@gmail.com>
2021-05-11 14:14:03 +0200

    User Registration pt.2

--- src/main/java/com/techvology/ppmtfullstack/services/UserService.java
+++ src/main/java/com/techvology/ppmtfullstack/services/UserService.java
@@ -1,6 +1,7 @@
 package com.techvology.ppmtfullstack.services;
 
 import com.techvology.ppmtfullstack.domain.User;
+import com.techvology.ppmtfullstack.exceptions.UsernameAlreadyExistsException;
 import com.techvology.ppmtfullstack.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -9,17 +10,25 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
 
-    @Autowired
-    private UserRepository userRepository;  //TODO: Constructor injection to be done
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Autowired
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+
 
     public User saveUser(User newUser){
-        newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
+        try {
+            newUser.setUsername(newUser.getUsername());
+            newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
 
-
-
-        return userRepository.save(newUser);
+            return userRepository.save(newUser);
+        }catch (Exception ex) {
+            throw new UsernameAlreadyExistsException("Username '"+newUser.getUsername()+"' already exists!");
+        }
     }
 }
