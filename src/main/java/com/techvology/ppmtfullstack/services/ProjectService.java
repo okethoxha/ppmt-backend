348eb63eacc53a3cdbbab192539e3afdd9634954
Oket <oket.hoxha@gmail.com>
2021-05-12 20:40:21 +0200

    Update functionality completed for project for a specific user.

--- src/main/java/com/techvology/ppmtfullstack/services/ProjectService.java
+++ src/main/java/com/techvology/ppmtfullstack/services/ProjectService.java
@@ -14,19 +14,34 @@ import org.springframework.stereotype.Service;
 @Service
 public class ProjectService {
 
-    @Autowired
-    private ProjectRepository projectRepository;
+
+    private final ProjectRepository projectRepository;
+
+    private final BacklogRepository backlogRepository;
+
+    private final UserRepository userRepository;
 
     @Autowired
-    private BacklogRepository backlogRepository;
-
-    @Autowired
-    private UserRepository userRepository;
+    public ProjectService(ProjectRepository projectRepository, BacklogRepository backlogRepository, UserRepository userRepository) {
+        this.projectRepository = projectRepository;
+        this.backlogRepository = backlogRepository;
+        this.userRepository = userRepository;
+    }
 
     public Project saveOrUpdateProject(Project project, String username){
+
+        if(project.getId() != null){
+            Project existingProject = projectRepository.findByProjectIdentifier(project.getProjectIdentifier());
+            if(existingProject !=null &&(!existingProject.getProjectLeader().equals(username))){
+                throw new ProjectNotFoundException("Project not found in your account");
+            }else if(existingProject == null){
+                throw new ProjectNotFoundException("Project with ID: '"+project.getProjectIdentifier()+"' cannot be updated because it doesn't exist");
+            }
+        }
+
         try{
 
-            User user =  userRepository.findByUsername(username);
+            User user = userRepository.findByUsername(username);
             project.setUser(user);
             project.setProjectLeader(user.getUsername());
             project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
@@ -53,15 +68,19 @@ public class ProjectService {
 
     public Project findProjectByIdentifier(String projectId, String username){
 
+        //Only want to return the project if the user looking for it is the owner
+
         Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
 
         if(project == null){
             throw new ProjectIdException("Project ID '"+projectId+"' does not exist");
+
         }
 
         if(!project.getProjectLeader().equals(username)){
-            throw new ProjectNotFoundException("Project not found in your account!");
+            throw new ProjectNotFoundException("Project not found in your account");
         }
+
 
 
         return project;
